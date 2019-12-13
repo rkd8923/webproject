d9fa1b938bdb7b41927402fc8fccaa3a53b3c4b7
yb <kimuyb@snu.ac.kr>
2019-12-13 22:30:50 +0900

    Update Canvas.js

--- src/components/draw/Canvas.js
+++ src/components/draw/Canvas.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import CanvasDraw from 'react-canvas-draw';
 
+// class DrawingSettings {
+//   this.bru
+// }
+
+
 function Canvas() {
   return (
     <div>
-      <CanvasDraw />
+      <CanvasDraw brushColor={brushColor} brushRadius={brushRadius}  />
+      
 
     </div>
   );
