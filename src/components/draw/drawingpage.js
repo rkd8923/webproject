c8e66c4efac5b3f67d047ae998fc3b5e97d6356a
yb <kimuyb@snu.ac.kr>
2019-12-13 19:28:05 +0900

    Splitting Canvas from drawingpage

--- src/components/draw/drawingpage.js
+++ src/components/draw/drawingpage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CanvasDraw from 'react-canvas-draw';
+import Canvas from './Canvas'
 
 function DrawingPage() {
   return (
     <div>
+      <div>
+      Drawing Canvas
+      </div>
+      <input type="button" />
       <CanvasDraw />
     </div>
   );
