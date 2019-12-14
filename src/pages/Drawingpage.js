9bfa9f2c6fb3faadab4e2a85beffab3884b4fe1b
Jiwon Kang <rkd8923@gmail.com>
2019-12-14 14:37:30 +0900

    add route

--- src/pages/Drawingpage.js
+++ src/pages/Drawingpage.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Canvas from '../components/Canvas';
-import DrawingSubject from '../components/DrawingSubject';
-import DrawingTools from '../components/DrawingTools';
-import SubmitDrawing from '../components/SubmitDrawing';
+// import Canvas from '../components/Canvas';
+// import DrawingSubject from '../components/DrawingSubject';
+// import DrawingTools from '../components/DrawingTools';
+// import SubmitDrawing from '../components/SubmitDrawing';
 
 
 function DrawingPage() {
   return (
     <div>
-      <div>
+      {/* <div>
       Drawing Canvas
       </div>
       <DrawingSubject />
       <Canvas />
-      <SubmitDrawing />
+      <SubmitDrawing /> */}
     </div>
   );
 }
