8f55fe744d2f50527400332b2f8bd732dd4aa771
Jiwon Kang <rkd8923@gmail.com>
2019-12-14 11:32:57 +0900

    init login page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import firebase from './firebase';
 import firebaseStorage from './firebase.storage';
 function App() {
   const [user, setUser] = useState();
-  const googleLogin = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
-  const logout = async () => {
-    firebase.auth().signOut();
-  }
+
   firebase.auth().onAuthStateChanged((user) => {
     console.log('test', user);
     if (user) {
