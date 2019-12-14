6702d3c5da42d544137d883a7d0871e6a7921303
yb <kimuyb@snu.ac.kr>
2019-12-14 14:21:26 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import FirebaseTest from './components/FirebaseTest';
-import firebase from './firebase';
-import firebaseStorage from './firebase.storage';
 import './styles/App.css';
+import DrawingPage from './pages/Drawingpage';
 
 function App() {
-  const [user, setUser] = useState();
-
-  firebase.auth().onAuthStateChanged((user) => {
-    console.log('test', user);
-    if (user) {
-      setUser(user);
-      console.log('user', user);
-    } else {
-      setUser();
-      console.log('loginX');
-    }
-  })
 
   return (
     <div>
-      <span>TEST</span>
+      <DrawingPage />
     </div>
   );
 }
