6ef56d532c43f3d2ce73defac5123383002c9dc1
Kyeongwoo Nam <nkw0730@naver.com>
2019-12-14 14:38:18 +0900

    tt

--- src/pages/Solve.js
+++ src/pages/Solve.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
 import React, { useState, useEffect } from 'react';
 
-function App() {
+function Solve() {
+  let time = 0;
+  setInterval (() => {
+    const timer = document.getElementById('timer');
+    time++;
+    timer.textContent = `Time: ${time}`;
+  }, 1000)
+
   const onAnswer = (e) =>{
     e.preventDefault();
     
-    const answerWin = window.open('', 'window', 'width=100, height=200, left= 650, top=300');
-    answerWin.document.write("<p>12321</p>");
+    window.open('src/components/solve/answerPop.html', 'window', 'width=100, height=200, left= 650, top=300');
   }
 
   const onGiveup = (e) =>{
@@ -20,7 +24,7 @@ function App() {
   return (
     <div>
       <div style = {{textAlign : 'center'}}>
-        <img src= {require('./20101209012239.jpg')} alt ='cy' />
+        <img src= {require('C:/Users/nkw07/Desktop/webproject/src/20101209012239.jpg')} alt ='cy' />
       </div>
       <div id = 'timer' style = {{textAlign : 'right',  marginRight : '100px'}}>Time: 0
         </div>
@@ -33,11 +37,11 @@ function App() {
         </form>
 
         <form onSubmit= {onGiveup} style = {{textAlign : 'right', float : 'right'}}>
-          <input name = 'giveup' type = 'submit' value= '포기' style = {{marginRight : '100px'}}/>
+          <input type = 'submit' value= '포기' style = {{marginRight : '100px'}}/>
         </form>
     </div>
   );
 }
 
 
-export default App;
+export default Solve;
