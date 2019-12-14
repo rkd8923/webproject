7e8c29f53d0191cf8e794d969b0780f48e84cd13
Jiwon Kang <rkd8923@gmail.com>
2019-12-14 17:48:47 +0900

    login form

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,54 +1,53 @@
 import React from "react";
 import firebaseDb from '../../firebase.db';
 
-
 const pushUserData = async (nickname) => {
-    firebaseDb.pushUserData({
-      name: nickname,
-    });
-  }
-
+  firebaseDb.pushUserData({
+    id: localStorage.getItem('userEmail'),
+    name: nickname,
+    score: 0,
+  });
+}
 class LoginForm extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        nickname: "",
-      };
-    }
-      handleNickname = e => {
-        e.preventDefault();
-        this.setState({
-          nickname: e.target.value
-        });
-      };
-    
-      render() {
-        return (
-            <form className="LoginForm"> 
-                <div className="LoginFormTitle">
-                    <h1>닉네임 생성</h1>
-                </div>
-                <div className="Content">
-                    <div className="ContentNickname">
-                        닉네임 : <input 
-                        placeholder="닉네임 입력"
-                        value={this.state.nickname}
-                        onChange={this.handleNickname}
-                        />
-                    </div>
-                    <div className="CreateNickname">
-                        <input
-                        type="button"
-                        value="생성"
-                        onClick= {() => {
-                            console.log(this.state.nickname);
-                            pushUserData(this.state.nickname);
-                          }}/>
-                    </div>
-                </div>
-            </form>
-            );
-     }
+    this.state = {
+      nickname: "",
+    };
+  }
+  handleNickname = e => {
+    e.preventDefault();
+    this.setState({
+      nickname: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form className="LoginForm"> 
+        <div className="LoginFormTitle">
+          <h1>닉네임 생성</h1>
+        </div>
+        <div className="Content">
+          <div className="ContentNickname">
+            닉네임 : <input 
+              placeholder="닉네임 입력"
+              value={this.state.nickname}
+              onChange={this.handleNickname}
+            />
+          </div>
+          <div className="CreateNickname">
+            <input
+            type="button"
+            value="생성"
+            onClick= {() => {
+              pushUserData(this.state.nickname);
+            }}/>
+          </div>
+        </div>
+      </form>
+    );
+  }
  }
 export default LoginForm;
