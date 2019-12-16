97dd1ebe4c74cb92ace48d25e717ab3559cf53e7
Kyeongwoo Nam <nkw0730@naver.com>
2019-12-16 09:49:40 +0900

    21

--- src/pages/Solve.js
+++ src/pages/Solve.js
@@ -2,10 +2,25 @@ import React, { useState, useEffect, useCallback } from 'react';
 import AnswerPop from '../components/solve/answerPop.js'
 import firebaseDb from '../firebase.db.js';
 import { database } from 'firebase';
+import '../styles/Solve.css';
 
-
-function Solve() {
+// timer는 useEffect 이용해서 구현해야함.
+function Solve(props) {
+  const [problem, setProblem] = useState(); // problem은 랜덤으로 선택된 이번에 출제될 문제의 오브젝트
   const [paints, setPaints] = useState([]);
+  const [answer, setAnswer] = useState('');
+  const [myData, setMyData] = useState(); // myData는 { id, name, score, solved }를 저장 
+                                          //solved는 내가 해결한 문제의 id들의 list
+  const setMyDatas = async () => {
+    if (props.user) {
+      const my = await firebaseDb.getMyData(props.user.email);
+      setMyData(my);
+    }
+  };
+  const handleAnswer = (e) => {
+    e.preventDefault();
+    setAnswer(e.target.value);
+  };
   // let time = 0;
   // setInterval (() => {
   //   const timer = document.getElementById('timer');
@@ -17,6 +32,20 @@ function Solve() {
     setPaints(Object.values(response));
   }, [paints]);
 
+  const selectRandomImage = () => {
+    // paints에 있는 이미지오브젝트들 중에서
+    // myData.solved 에는 없는 이미지 중, random으로 하나 선택해서 
+    // setProblem 하기
+    console.log('구현예정');
+  }
+
+  const submit = () => {
+    // imageObject의 정답과 확인 후 정답 여부 알려주기
+    // 만약 정답일 경우, user의 데이터(score, solved) 해결해서 push
+  }
+  const giveUp = () => {
+    // 포기 시 다시 home
+  }
   const onAnswer = (e) =>{
     e.preventDefault();
     
@@ -32,25 +61,38 @@ function Solve() {
   } 
   useEffect(() => {
     getImageData();
-  }, [])
+  }, []);
   useEffect(() => {
     console.log(paints);
-  }, [paints])
+  }, [paints]);
+  
+  // user에 저장된 email을 이용해 myData를 업데이트
+  useEffect(() => { 
+    setMyDatas();
+  }, [props.user]);
+  useEffect(() => {
+    selectRandomImage();
+  }, [myData]);
   return (
-    <div>
-      <div style = {{textAlign : 'center'}}>
-        <img src= {require('../20101209012239.jpg')} alt ='cy' />
+    <div id="solve">
+      <div id="canvas-box">
+        <span>여기는 캔버스로 채울 예정</span>
       </div>
-      <div id = 'timer' style = {{textAlign : 'right',  marginRight : '100px'}}>Time: 0
+      <div id="solve-tools">
+        <div className="timer">time: 0</div>
+        <div className="answer-box">
+          <input 
+            className='answer'
+            type='text'
+            value={answer}
+            onChange={handleAnswer}
+          />
+          <button onClick={submit}>제출</button>
         </div>
-        <form onSubmit={onAnswer} style = {{textAlign : 'center'}}>
-          <input type = 'text' className = 'answer' />
-          <input type = 'submit' value='입력' />
-        </form>
-
-        <form onSubmit= {onGiveup} style = {{textAlign : 'right', float : 'right'}}>
-          <input type = 'submit' value= '포기' style = {{marginRight : '100px'}}/>
-        </form>
+        <button className="give-up" onClick={giveUp}>
+          포기
+        </button>
+      </div>
     </div>
   );
 }
