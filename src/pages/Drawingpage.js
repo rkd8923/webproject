d7f53ba30911da91999da7ad87f54f89d15dfac8
yb <kimuyb@snu.ac.kr>
2019-12-17 16:09:13 +0900

    Update Drawingpage.js

--- src/pages/Drawingpage.js
+++ src/pages/Drawingpage.js
@@ -1,23 +1,31 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Canvas from '../components/draw/Canvas';
 import SubmitDrawing from '../components/draw/SubmitDrawing';
 
 function DrawingPage(props) {
-  useEffect(() => {
-    console.log(props.user);
-    if (props.user) {
-      console.log(props.user.email);
-    }
-  }, [props.user])
+  let userEmail = '';
+  if (props.user) {
+    userEmail = props.user.email;
+    console.log('yoyo', userEmail);
+  }
+  // useEffect(() => {
+  //   if (props.user) {
+  //     userEmail = props.user.email;
+  //     console.log('yoyo', userEmail);
+  //   }
+  // }, [props.user]);
+
   return (
     <div>
       <div>
       Drawing Canvas
       </div>
-      <Canvas />
+      <Canvas userEmail={userEmail} />
       <SubmitDrawing />
     </div>
   );
 }
+
 
 export default DrawingPage;
