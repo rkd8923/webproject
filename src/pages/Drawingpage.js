385d85498eae8159df7d4f2d4f8b7f3e67f843e7
yb <kimuyb@snu.ac.kr>
2019-12-19 01:43:30 +0900

    styling

--- src/pages/Drawingpage.js
+++ src/pages/Drawingpage.js
@@ -2,18 +2,27 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import CanvasDraw from 'react-canvas-draw';
 import Button from '@material-ui/core/Button';
-import { withStyles,makeStyles  } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import {
+  red, green, blue, yellow, grey,
+} from '@material-ui/core/colors';
+import Drawer from '@material-ui/core/Drawer';
+import Container from '@material-ui/core/Container';
 import firebaseDb from '../firebase.db';
+import TextField from '@material-ui/core/TextField';
 
+const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(3),
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
   },
 }));
 
@@ -29,12 +38,56 @@ function DrawingPage(props) {
   const [brushColor, setBrushColor] = useState('#000000');
   const [answer, setAnswer] = useState('');
 
+  const ColorButtonRed = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText(red[500]),
+      backgroundColor: red[500],
+      '&:hover': {
+        backgroundColor: red[700],
+      },
+    },
+  }))(Button);
+
+  const ColorButtonGreen = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText(green[500]),
+      backgroundColor: green[500],
+      '&:hover': {
+        backgroundColor: green[700],
+      },
+    },
+  }))(Button);
+  const ColorButtonBlue = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText(blue[500]),
+      backgroundColor: blue[500],
+      '&:hover': {
+        backgroundColor: blue[700],
+      },
+    },
+  }))(Button);
+  const ColorButtonYellow = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText(yellow[500]),
+      backgroundColor: yellow[500],
+      '&:hover': {
+        backgroundColor: yellow[700],
+      },
+    },
+  }))(Button);
+  const ColorButtonBlack = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText(grey[500]),
+      backgroundColor: grey[500],
+      '&:hover': {
+        backgroundColor: grey[700],
+      },
+    },
+  }))(Button);
+
 
   const classes = useStyles();
 
-  const handleChange = (event) => {
-    setBrushColor(event.target.value);
-  };
 
   if (props.user) { // 로그인체크
     userEmail = props.user.email;
@@ -50,60 +103,63 @@ function DrawingPage(props) {
   function DrawingTools() {
     return (
       <div>
-        <FormControl component="fieldset" className={classes.formControl}>
-          <FormLabel component="legend">Brush Color</FormLabel>
-          <RadioGroup aria-label="gender" name="colors" value={brushColor} onChange={handleChange}>
-            <FormControlLabel value="#e62b12" control={<Radio />} label="Red" />
-            <FormControlLabel value="#32a628" control={<Radio />} label="Green" />
-            <FormControlLabel value="#0022ff" control={<Radio />} label="Blue" />
-            <FormControlLabel
-              value="disabled"
-              disabled
-              control={<Radio />}
-              label="(Disabled option)"
-            />
-          </RadioGroup>
-        </FormControl>
-        <input
-          id="red"
-          value="red"
-          type="button"
-          onClick={() => {
-            setBrushColor('#e62b12');
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
           }}
-        />
-        <input
-          id="green"
-          value="green"
-          type="button"
-          onClick={() => {
-            setBrushColor('#32a628');
-          }}
-        />
-        <input
-          id="blue"
-          value="blue"
-          type="button"
-          onClick={() => {
-            setBrushColor('#0022ff');
-          }}
-        />
-        <input
-          id="yellow"
-          value="yellow"
-          type="button"
-          onClick={() => {
-            setBrushColor('#ffea00');
-          }}
-        />
-        <input
-          id="black"
-          value="black"
-          type="button"
-          onClick={() => {
-            setBrushColor('#000000');
-          }}
-        />
+          anchor="left"
+        >
+          <ButtonGroup
+            orientation="vertical"
+            color="primary"
+            aria-label="vertical outlined primary button group"
+          >
+            <ColorButtonRed
+              variant="contained"
+              onClick={() => {
+                setBrushColor('#e62b12');
+              }}
+            >
+            Red
+            </ColorButtonRed>
+            <ColorButtonGreen
+              variant="contained"
+              onClick={() => {
+                setBrushColor('#32a628');
+              }}
+            >
+            Green
+            </ColorButtonGreen>
+            <ColorButtonBlue
+              variant="contained"
+              onClick={() => {
+                setBrushColor('#0022ff');
+              }}
+            >
+            Blue
+            </ColorButtonBlue>
+            <ColorButtonYellow
+              variant="contained"
+              onClick={() => {
+                setBrushColor('#ffea00');
+              }}
+            >
+            Yellow
+            </ColorButtonYellow>
+            <ColorButtonBlack
+              variant="contained"
+              onClick={() => {
+                setBrushColor('#000000');
+              }}
+            >
+            Black
+            </ColorButtonBlack>
+          </ButtonGroup>
+        </Drawer>
+
+
         <input
           id="eraser"
           value="eraser"
@@ -138,33 +194,48 @@ function DrawingPage(props) {
       <div>
       Drawing Canvas
       </div>
-      <DrawingTools />
-      <CanvasDraw
-        hideGrid
-        ref={(canvasDraw) => (currentDrawing = canvasDraw)}
-        brushColor={brushColor}
-        brushRadius={brushRadius}
-        canvasWidth={canvasWidth}
-        canvasHeight={canvasHeight}
-        lazyRadius={lazyRadius}
-        DrawingTools={DrawingTools}
-        // saveData={localStorage.getItem('savedDrawing')}
-      />
-      <form
-        id="drawing-submission"
-        onSubmit={(e) => {
-          e.preventDefault();
-          setAnswer(e.target.value);
-          onSend();
-          // localStorage.setItem('savedDrawing', image);
-        }}
-      >
-        <input type="text" id="answer-input" value={answer} onChange={(e) => setAnswer(e.target.value)} />
-        <input
-          type="submit"
-          value="save"
+      <div>
+        <DrawingTools />
+      </div>
+      <Container fixed>
+        <CanvasDraw
+          hideGrid
+          ref={(canvasDraw) => (currentDrawing = canvasDraw)}
+          brushColor={brushColor}
+          brushRadius={brushRadius}
+          canvasWidth={canvasWidth}
+          canvasHeight={canvasHeight}
+          lazyRadius={lazyRadius}
+          DrawingTools={DrawingTools}
+          // saveData={localStorage.getItem('savedDrawing')}
         />
-      </form>
+      </Container>
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          rowsMax="4"
+          value={value}
+          onChange={handleChange}
+          variant="outlined"
+        />
+        <form
+          id="drawing-submission"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setAnswer(e.target.value);
+            onSend();
+            // localStorage.setItem('savedDrawing', image);
+          }}
+        >
+          <input type="text" id="answer-input" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+          <input
+            type="submit"
+            value="save"
+          />
+        </form>
+      </div>
 
       {/* <CanvasDraw
         disabled
